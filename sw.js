69f833f533696c0589c067e77ba4e8bd58f23448
kimiti7 <gideonkibe7@gmail.com>
2026-08-15 15:00:25 +0300

    Version updater

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'site-cache-v1';
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
