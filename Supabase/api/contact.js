14e0806acd18bfbe7cd90d244abb36ddc010a4a7
kimiti7 <gideonkibe7@gmail.com>
2026-08-17 11:45:13 +0300

    supabase

---
+++ Supabase/api/contact.js
@@ -0,0 +1,27 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method not allowed" });
+    }
+
+    const { email, name, phone, message } = req.body;
+
+    const { data, error } = await supabase
+        .from("contact_information")
+        .insert([{ email, name, phone, message }]);
+
+    if (error) {
+        return res.status(500).json({ 
+            message: "Error saving data",
+            error: error.message  
+        });
+    }
+
+    res.status(200).json({ message: "Message received!" });
+}
